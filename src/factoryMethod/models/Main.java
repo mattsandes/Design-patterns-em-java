c3370a22f10d5a099b583699821aaf449a4d6bf3
Mateus Sandes <mateussandes@hotmail.com.br>
2024-03-29 21:13:45 -0300

    testando pilotoFActory

--- src/factoryMethod/models/Main.java
+++ src/factoryMethod/models/Main.java
@@ -1,6 +1,7 @@
 package factoryMethod.models;
 
 import factoryMethod.factories.JogadorFactory;
+import factoryMethod.factories.PilotoFactory;
 import factoryMethod.factories.ProgramadorFactory;
 import factoryMethod.interfaces.Pessoa;
 
@@ -9,16 +10,23 @@ public class Main {
 		
 		//criando o tipo programador sem instanciar;
 		ProgramadorFactory factory = new ProgramadorFactory();
+		JogadorFactory jFactory = new JogadorFactory();
+		PilotoFactory pFactory = new PilotoFactory();
 		
 		Pessoa pessoa = factory.createProgramador();
 		
 		System.out.println(pessoa);
 		
 		//criando o tipo jogador sem instanciar;
-		JogadorFactory jFactory = new JogadorFactory();
 		
 		pessoa = jFactory.createJogador();
 		
 		System.out.println(pessoa);
+		
+		//Criando um pilot sem precisar instanciar;
+		pessoa = pFactory.createPiloto();
+		
+		System.out.println(pessoa);
+		
 	}
 }
